023e28cc47b06cd91829df1acc51795cfaf21582
rajputnishant29 <nishantrajput7017@gmail.com>
2025-07-06 22:29:48 +0530

    oks

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-   base: '/Travel-Planner/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
