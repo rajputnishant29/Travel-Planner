a7266efb5fb0f394be2d4cdbbf6b797a26688621
rajputnishant29 <nishantrajput7017@gmail.com>
2025-07-06 22:15:27 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+   base: '/Travel-Planner/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
